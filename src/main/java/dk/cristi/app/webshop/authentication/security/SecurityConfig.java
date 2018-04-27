51ffe66b4932605e44a994faaa2c6c5ddd786ebb
Cristian Betivu <cbetivu@gmail.com>
2018-04-27 10:49:12 +0200

    Initialize project, add basic security

---
+++ src/main/java/dk/cristi/app/webshop/authentication/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package dk.cristi.app.webshop.authentication.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder().username("john.carnell").password("password").roles("USER").build())
+                .withUser(User.withDefaultPasswordEncoder().username("william").password("hispass").roles("ADMIN").build());
+    }
+}
