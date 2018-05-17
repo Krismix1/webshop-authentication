13c03c3a53fc5ea541122d353967eaacb1e8fc9e
Cristian Betivu <cbetivu@gmail.com>
2018-05-17 23:03:54 +0200

    Cloud config and eureka client

--- src/main/java/dk/cristi/app/webshop/authentication/WebShopAuthenticationApplication.java
+++ src/main/java/dk/cristi/app/webshop/authentication/WebShopAuthenticationApplication.java
@@ -2,6 +2,7 @@ package dk.cristi.app.webshop.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -17,6 +18,7 @@ import java.util.Map;
 @RestController
 @EnableResourceServer
 @EnableAuthorizationServer
+@EnableEurekaClient
 public class WebShopAuthenticationApplication {
 
     @RequestMapping(value = {"/user"}, produces = {MimeTypeUtils.APPLICATION_JSON_VALUE})
