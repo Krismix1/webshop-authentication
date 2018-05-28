0e9c95af6d8fd143ce2ccd01176c53ff9c8b8e90
Epharas <cris0922@stud.kea.dk>
2018-05-28 02:04:11 +0200

    More properties files and docker deployment

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+# Wait for config server
+status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
+until [ "$status" = "UP" ]; do
+    echo "Waiting for config server"
+    sleep 1
+    status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
+done
 # Wait for discovery server
-status=$(curl http://$DISCOVERY_IP:$DISCOVERY_PORT | jq -r '.error')
-until [ "$status" = "unauthorized" ]; do
+status=$(curl http://discoveryserver:8080/actuator/health | jq -r '.status')
+until [ "$status" = "UP" ]; do
     echo "Waiting for discovery server"
     sleep 1
-    status=$(curl http://$DISCOVERY_IP:$DISCOVERY_PORT | jq -r '.error')
+    status=$(curl http://discoveryserver:8080/actuator/health | jq -r '.status')
 done
 # Wait for gateway server
-status=$(curl http://$GATEWAY_IP:$GATEWAY_PORT | jq -r '.error')
-until [ "$status" = "unauthorized" ]; do
+status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
+until [ "$status" = "UP" ]; do
     echo "Waiting for gateway server"
     sleep 1
-    status=$(curl http://$GATEWAY_IP:$GATEWAY_PORT | jq -r '.error')
+    status=$(curl http://configserver:8080/actuator/health | jq -r '.status')
 done
-echo "Server is running"
+echo "Starting auth server"
 # Run tomcat
 /usr/local/tomcat/bin/catalina.sh run
